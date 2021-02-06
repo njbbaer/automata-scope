88d658ea7f2a31bce4477a3bb9deb8d39f72cd73
Nate Baer <njbbaer@gmail.com>
2021-02-05 23:29:47 -0800

    Make deploy script accept address argument

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-rsync -a /home/nate/autoscope pi@192.168.1.96:/home/pi
-ssh pi@192.168.1.96 -t "python3 /home/pi/autoscope/autoscope"
+address=$1
+rsync -a /home/nate/autoscope pi@$address:/home/pi
+ssh pi@$address -t "python3 /home/pi/autoscope/autoscope"
