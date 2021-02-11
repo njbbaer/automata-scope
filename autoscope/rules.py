d33e3cf8be0adc78aac0483056284844731e7159
Nate Baer <njbbaer@gmail.com>
2021-02-10 19:28:12 -0800

    Create square seed type

--- autoscope/rules.py
+++ autoscope/rules.py
@@ -44,7 +44,27 @@ class RandomSeed:
         return new_board.astype(np.int)
 
     def name(self):
-        return f'random {self.density}'
+        return f'random d={self.density}'
+
+class SquareSeed:
+    def __init__(self, side):
+        self.shape = [side, side]
+
+    def populate(self, board):
+        new_board = np.zeros(shape=board.shape, dtype=int)
+        square = np.ones(shape=self.shape, dtype=int)
+        pos = [
+            int(board.shape[0]/2 - square.shape[0]/2),
+            int(board.shape[1]/2 - square.shape[1]/2)
+        ]
+        new_board[
+            pos[0]:pos[0]+self.shape[0],
+            pos[1]:pos[1]+self.shape[1]
+        ] = square
+        return new_board
+
+    def name(self):
+        return f'square s={self.shape[0]}'
 
 Rule = namedtuple("Rule", "name rule neighborhood seeds")
 
@@ -62,7 +82,7 @@ rules = [
     Rule(name = "assimilation",
          rule = [[4, 5, 6, 7], [3, 4, 5]],
          neighborhood = _moore_neighborhood(),
-         seeds = [RandomSeed(0.18)]),
+         seeds = [RandomSeed(0.17)]),
 
     Rule(name = "coagulations",
          rule = [[2, 3, 5, 6, 7, 8], [3, 7, 8]],
@@ -92,20 +112,15 @@ rules = [
     Rule(name = "flakes",
          rule = [[0, 1, 2, 3, 4, 5, 6, 7, 8], [3]],
          neighborhood = _moore_neighborhood(),
-         seeds = [RandomSeed(0.01),
-                  RandomSeed(0.1),
-                  RandomSeed(0.5),
-                  RandomSeed(0.9),
-                  RandomSeed(0.99)]),
+         seeds = [SquareSeed(30)]),
 
     Rule(name = "gnarl",
          rule = [[1], [1]],
          neighborhood = _moore_neighborhood(),
-         seeds = [RandomSeed(0.01),
-                  RandomSeed(0.1),
-                  RandomSeed(0.5),
-                  RandomSeed(0.9),
-                  RandomSeed(0.99)]),
+         seeds = [SquareSeed(1),
+                  SquareSeed(2),
+                  SquareSeed(4),
+                  SquareSeed(5)]),
 
     Rule(name = "high_life",
          rule = [[2, 3], [3, 6]],
